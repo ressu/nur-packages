a0c03d4a28b5e4bda4a14544c8170095df60c0c3
Sami Haahtinen <ressu@ressukka.net>
2025-10-29 11:18:35 +0000

    Make maintainers available to all packages
    This change applies an overlay to the pkgs instance in flake.nix to
    extend the lib.maintainers set with the custom maintainers defined in
    lib/maintainers.nix. This makes the custom maintainers available to all
    packages in the repository by default, so they don't need to be passed
    explicitly to each package.
    
    This fixes the build error where the ressu maintainer was not found.

--- flake.nix
+++ flake.nix
@@ -6,14 +6,35 @@
       forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
     in
     {
-      legacyPackages = forAllSystems (system: import ./default.nix {
-        pkgs = import nixpkgs { inherit system; };
-      });
+      legacyPackages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              (self: super: {
+                lib = super.lib.extend (libself: libsuper: {
+                  maintainers = libsuper.maintainers // (import ./lib/maintainers.nix);
+                });
+              })
+            ];
+          };
+        in
+        import ./default.nix { inherit pkgs; }
+      );
       packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
 
       devShells = forAllSystems (system:
         let
-          pkgs = import nixpkgs { inherit system; };
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              (self: super: {
+                lib = super.lib.extend (libself: libsuper: {
+                  maintainers = libsuper.maintainers // (import ./lib/maintainers.nix);
+                });
+              })
+            ];
+          };
         in
         {
           default = pkgs.mkShell {
